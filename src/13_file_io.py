a3c6e61bdebb03d47613d15faa502f16cfe8bb74
cjgodfather <chaoji@Chaos-MacBook-Pro.local>
2020-02-11 13:10:11 -0500

    finished rest practices

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -7,12 +7,22 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
+f = open("foo.txt")
+
+print(f.read())
+f.close()
 
 # YOUR CODE HERE
 
+w = open("bar.txt", "w")
+w.write("this is line1\n")
+w.write("this is line2\n")
+w.write("this is line3\n")
+w.close()
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
-# then close the file. Open up "bar.txt" and inspect it to make 
+# then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
--- src/14_cal.py
+++ src/14_cal.py
@@ -4,7 +4,7 @@ render a calendar to your terminal.
 https://docs.python.org/3.6/library/calendar.html
 
 Write a program that accepts user input of the form
-  `14_cal.py month [year]`
+  `14_cal.py [month] [year]`
 and does the following:
  - If the user doesn't specify any input, your program should
    print the calendar for the current month. The 'datetime'
@@ -22,3 +22,16 @@ and does the following:
 import sys
 import calendar
 from datetime import datetime
+
+
+i = input("14_cal.py [month] [year] ")
+
+
+try:
+    month, year = i.split()
+    # print(type(month), type(year))
+    print(calendar.month(int(year), int(month)))
+
+except(ValueError):
+
+    print(calendar.month(datetime.today().year, datetime.today().month))
